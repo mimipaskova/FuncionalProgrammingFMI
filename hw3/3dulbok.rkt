ed1822d36fe8462ae73ddc1817f01da7949a4a2f
mimipaskova <maria.paskova3@gmail.com>
2015-11-19 10:18:42 +0200

    Add 3 task for hw3

---
+++ hw3/3dulbok.rkt
@@ -0,0 +1,16 @@
+(define (atom? x) (not (pair? x)))
+
+
+(define (deep-fold nv term op l)
+  (cond ((null? l) nv)
+        ((atom? l) (term l))
+        (else (op (deep-fold nv term op (car l))
+                  (deep-fold nv term op (cdr l))))))
+
+(define (insert-after-deep x lst)
+  (cond ((null? lst) '())
+        ((atom? (car lst)) (if (even? (car lst)) (append (insert-after-deep x (car lst)) (list x))))
+        (else (insert-after-deep x (car lst)) (insert-after-deep x (cdr lst)))))
+        
+
+(insert-after-deep 10 '(1 ((2) 3 4) (((5))) 6 7))
