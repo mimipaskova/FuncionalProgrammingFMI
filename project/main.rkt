43a493a1304bf20d6ae67c2d7c8aaf675221c7de
mimipaskova <maria.paskova3@gmail.com>
2016-01-14 20:50:06 +0200

    Refactoring name of functions and output. Not ready.

--- project/main.rkt
+++ project/main.rkt
@@ -1,93 +1,58 @@
-(define lstt '(("mimi" "s" "m") ("mimi" "s" "mn") ("mimi" "sr" "m")))
-
-; Input - list with words where third is label for the kind of word (m,j,sr,mn)
-; Output - list with words with equal third value
+;Филтрира по втория етикет (род, число)
 (define (filterBySecondLabel lst kind)
   (filter (lambda (x) (equal? kind (car (cdr (cdr x))))) lst))
 
+;Филтрира по пътвия етикет вид на думата (п,о,с,д)
 (define (filterByFirstLabel lst kind)
   (filter (lambda (x) (equal? kind (car (cdr x)))) lst))
 
-;(filterBySecondLabel lstt "m")
-;(filterByFirstLabel (filterBySecondLabel lstt "m") "s")
-
-;(define example '(("golqmoto" "o" "sr") ("kuche" "p" "sr") ("lae" "s" "ed") ("pticite" "d")
-;                                        ("zelenite" "o" "mn") ("jabi" "p" "mn") ("gledat" "s" "mn")
-;                                        ("kum nebeto" "d") ))
-
+;Четене на данни от файл data.txt
 (define in (open-input-file "data.txt"))
 (define example (read in))
 
+;намира всички определения
+(define (getAllAttributes lst)
+  (filter (lambda (x) (equal? "o" (car (cdr x)))) lst))
 
-(define (filterDupulnenia lst)
-  (filter (lambda (x) (not (equal? "d" (car (cdr x))))) lst))
+;намира всички подлози
+(define (getAllSubjects lst)
+  (filter (lambda (x) (equal? "p" (car (cdr x)))) lst))
 
-;(filterDupulnenia example)
+;намира всички сказуеми
+(define (getAllPredicates lst)
+  (filter (lambda (x) (equal? "s" (car (cdr x)))) lst))
 
-;Get all dopulnenia
-(define (getAllDopulnenia lst)
+;Намира всички допълнения
+(define (getAllObjects lst)
   (map (lambda (x) (car x)) (filterByFirstLabel lst "d")))
 
-;(getAllDopulnenia example)
-
-;from 0 to length-1
-;(random (length (getAllDopulnenia example)))
-
-;first element is with index 0
+;Взема n елемент на лист, като индексирането започва от 0
 (define (getNelement lst n)
   (if (= n 0) (car lst)
       (getNelement (cdr lst) (- n 1))))
 
-;get random word which is filtered by f (function)
+;Намира произволна дума, която е филтрирана чрез f (функция)
 (define (getRandom f lst)
   (getNelement (f lst) (random (length (f lst)))))
 
-;(getRandom getAllDopulnenia example)
-
-
-;;;Podlog i opredelenie
-;(define (getAllPodlog lst)
-;  (map (lambda (x) (car x)) (filterByFirstLabel lst "p")))
-
-(define (getAllPodlog lst)
-  (filter (lambda (x) (equal? "p" (car (cdr x)))) lst))
-
-(define (getAllOpredelenia lst)
-  (filter (lambda (x) (equal? "o" (car (cdr x)))) lst))
-
-(define (getAllSkazuemi lst)
-  (filter (lambda (x) (equal? "s" (car (cdr x)))) lst))
-
-;(getRandom getAllPodlog example)
 ;Problem - if there isn't kind mn
-(define (findOpredelenieByPodlogAndPodlog lst)
-  (let* [(podlog (getRandom getAllPodlog example))
+(define (generateSentenceWithoutObject lst)
+  (let* [(podlog (getRandom getAllSubjects example))
          (kind (caddr podlog))
          ;(skazuemo (if (> 1 0) 1 0))
-         (skazuemo (if (equal? kind "mn") (getRandom getAllSkazuemi (filterBySecondLabel (getAllSkazuemi example) "mn"))
-        (getRandom getAllSkazuemi (filterBySecondLabel (getAllSkazuemi example) "ed"))))
+         (skazuemo (if (equal? kind "mn") (getRandom getAllPredicates (filterBySecondLabel (getAllPredicates example) "mn"))
+        (getRandom getAllPredicates (filterBySecondLabel (getAllPredicates example) "ed"))))
          ]
-    ;(if (equal? kind "mn") (car (getRandom getAllSkazuemi (filterBySecondLabel (getAllSkazuemi example) "mn")))
-     ;   (car (getRandom getAllSkazuemi (filterBySecondLabel (getAllSkazuemi example) "ed"))))
-    ;(cons (car (filterBySecondLabel (getAllOpredelenia lst) kind)) (list podlog skazuemo))))
-(map (lambda (x) (car x)) (cons (car (filterBySecondLabel (getAllOpredelenia lst) kind)) (list podlog skazuemo)))))
+  (map car (list (car (filterBySecondLabel (getAllAttributes lst) kind)) podlog skazuemo))))
 
-(findOpredelenieByPodlogAndPodlog example)
+(define (generateRandomSentence lst)
+  (append (generateSentenceWithoutObject lst) (list (getRandom getAllObjects lst) '".")))
 
-(define (getOPD lst)
-  (append (findOpredelenieByPodlogAndPodlog lst) (list (getRandom getAllDopulnenia lst))))
+(define output (generateRandomSentence example))
+output
 
-(define output (getOPD example))
-
-(call-with-output-file "output.txt"
-                    (lambda (out)
-                      (write output out)))
-                      
-                      
-                      
 (define out (open-output-file "output.txt" 'append))
-(write output out)
-(write out)
+(write (apply string-append (map (lambda (x) (string-append x " ")) output)) out)
 (newline out)
 (close-output-port out)                      
 
