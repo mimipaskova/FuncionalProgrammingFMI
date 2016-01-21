620d9415c934caec5b2a947c70fd32cf0732f8f7
mimipaskova <maria.paskova3@gmail.com>
2016-01-21 13:39:55 +0200

    Add from 1 to 4 tasks for hw4

--- hw4/main.hs
+++ hw4/main.hs
@@ -1,11 +1,49 @@
+-- 1 zad
 prime 1 = False
---prime n = null [x | x<- [2.. (n-1)], n `mod` x == 0]
 prime n = null [x | x<- [2.. sqn], n `mod` x == 0]
 				where sqn = (floor(sqrt(fromIntegral n)))
 
-primes = filter prime [1..]
+primeSqSum start end =sum [x^2 | x<-(filter prime[start..end])]
 
-primeSqSum start end = [x^2 | x<-(filter prime[start..end])]
+-- 2 zad
+getNumber n = if (n `mod ` 2 == 0 ) then  (-1/(fromIntegral n)) else (1/(fromIntegral n))
+seriesSum n =  sum $ map getNumber (take n [1..])
 
 
--- seriesSum n = take n [1, -1/2, 1/3..]
+-- 3 zad
+slice 1 end lst = take end lst
+slice start end (h:t) = slice (start - 1) (end-1) t
+
+-- 4 zad
+removeNths n [] = []
+removeNths n lst = (take (n-1) lst) : removeNths n (drop (n) lst)
+
+removeNth n lst = foldr (++) [] $ removeNths n lst
+
+
+
+
+
+
+
+
+
+
+-- removeNth 1 (h:t) = t
+-- removeNth n (h:t) = h : removeNth (n-1) t
+
+-- getNths n end= [n, n+n..end]
+-- -- getNths n) (removeNth n lst)
+-- -- remove n lst = [x | y<-(getNths n (length lst)), x<-(removeNth y lst)]
+
+-- -- tova gurmi
+
+-- remove n lst = map  (\x -> removeNth x lst) (getNths n (length lst))
+
+-- -- remove n lst = map  product [n, n+n..]
+
+-- -- tova e vzeto ot drugade i gurmi
+-- sumProduct ls = sum $ map product ls
+
+
+
